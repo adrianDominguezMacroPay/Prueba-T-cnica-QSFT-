84d2117c25e0ca6ee3081d608599768584f621bc
adrianDominguezMacroPay <adrian.dominguez@macropay.mx>
2025-06-21 11:49:07 -0600

    Initial Commit

---
+++ PruebaTécnicaQSFT/PruebaTe_cnicaQSFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaTe_cnicaQSFTApp.swift
+//  PruebaTécnicaQSFT
+//
+//  Created by Adrian Pascual Dominguez Gomez on 21/06/25.
+//
+
+import SwiftUI
+
+@main
+struct PruebaTe_cnicaQSFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PruebaTécnicaQSFTTests/PruebaTe_cnicaQSFTTests.swift
@@ -0,0 +1,16 @@
+//
+//  PruebaTe_cnicaQSFTTests.swift
+//  PruebaTécnicaQSFTTests
+//
+//  Created by Adrian Pascual Dominguez Gomez on 21/06/25.
+//
+
+import Testing
+
+struct PruebaTe_cnicaQSFTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
